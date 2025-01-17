191025f720456cf9481cb2c5c334fc5c304ac87b
BugHunter-X <askzenai@gmail.com>
2025-01-17 16:52:04 +0100

    Add bugSolution.pl file

---
+++ bugSolution.pl
@@ -0,0 +1,1 @@
+my @array = (1, 2, 3);my $element;if ($#array >= 3) {  $element = $array[3];} else {  $element = undef; # Handle out-of-bounds access gracefully}print "Element at index 3: ", $element || 'Undefined'; #Handles undef gracefully
